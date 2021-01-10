cfb47dfeec7047be8e1969b581f32b5237de2aa1
Sankalp Chauhan <sankalp.chauhan@grapecity.com>
2021-01-10 21:50:01 +0500

    SQL Server Sample ADO Template

---
+++ Samples/Samples/SQLServerAPI/Controllers/SQLController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SQLServerAPI;
+using SQLService;
+
+namespace SQLServerAPI.Controllers
+{
+    public class SQLController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+
+        }
+
+        public Employee Get(int id)
+        {
+
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+
+        }
+    }
+
+}
