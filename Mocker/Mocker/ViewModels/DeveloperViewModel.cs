16548bdd2926ff08f1f155f23d9c7cca0e8e761a
Sankalp.Chauhan <sankalp.chauhan@grapecity.com>
2021-01-18 17:48:12 +0530

    Fix: ViewModel was not fetching data properly

--- Mocker/Mocker/ViewModels/DeveloperViewModel.cs
+++ Mocker/Mocker/ViewModels/DeveloperViewModel.cs
@@ -12,16 +12,23 @@ namespace Mocker.ViewModels
         public int DevId { get; set; }
         public string FullName { get; set; }
         public string UserId { get; set; }
-        public virtual ICollection<DevAppViewModel> DevApps { get; set; }
+        public virtual List<DevAppViewModel> DevApps { get; set; }
 
         public static implicit operator DeveloperViewModel(Developer v)
         {
+            List<DevAppViewModel> devApps = new List<DevAppViewModel>();
+            foreach(DevApp d in v.DevApps)
+            {
+                DevAppViewModel devAppViewModel = new DevAppViewModel();
+                devAppViewModel = d;
+                devApps.Add(d);
+            }
             return new DeveloperViewModel
             {
                 DevId = v.DevId,
                 FullName = v.FullName,
                 UserId = v.UserId,
-                DevApps = (ICollection<DevAppViewModel>)v.DevApps
+                DevApps = devApps
             };
         }
     }
